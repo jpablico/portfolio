99b1d4cce5515e649dbce3d6ae8f586dc69cde5e
Josh Pablico <jbpablico27@gmail.com>
2025-07-26 07:58:43 -0500

    GH

--- src/data/education.js
+++
@@ -1,9 +0,0 @@
-export const education = [
-  {
-    degree: 'Bachelor of Science, Computer Science',
-    school: 'Collin College',
-    period: 'Expected May 2026',
-    gpa: '3.14 / 4.0'
-  },
-  
-];
--- src/data/githubStats.js
+++
@@ -1,5 +0,0 @@
-export const githubStats = {
-  followers: 1247,
-  repositories: 89,
-  contributions: 1200
-};
